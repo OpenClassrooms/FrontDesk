5f4ed555befa78de4d3eadbc657e2ccdf0bda3b7
Killian Herbunot <killian.herbunot@openclassrooms.com>
2017-05-05 11:55:11 +0200

    update format date in isActivePlan

--- src/Models/Plan.php
+++ src/Models/Plan.php
@@ -92,8 +92,8 @@ abstract class Plan
     public function isActive()
     {
         return null !== $this->getEndDate()
-            && $this->getEndDate() > new \DateTime(Carbon::now()->toDateTimeString())
-            && $this->getStartDate() < new \DateTime(Carbon::now()->toDateTimeString());
+            && $this->getEndDate() > new \DateTime(Carbon::now()->toDateString())
+            && $this->getStartDate() < new \DateTime(Carbon::now()->toDateString());
     }
 
     /**
